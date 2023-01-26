28096ee8b60ed29d1050fa630d133f31c39d50b4
Yasmin Carvalho <yas@MacBook-Air-de-Yasmin.local>
2023-01-26 13:45:34 -0300

    feat: employee

--- employee.py
+++ employee.py
@@ -2,5 +2,12 @@ from person import Person
 
 class Employee(Person):
 
-    def __init__(self,office):
-        
+    def __init__(self,cpf,name,address,birth_date,office):
+        super().__init__(cpf,name,address,birth_date)
+        self.__office = office
+
+    def get_office(self):
+        return self.__office
+    
+    def set_office(self, office):
+        self.__office = office

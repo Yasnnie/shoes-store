70c02d28a854d3b31c1967f137f29ef56bf7ff9f
Yasmin Carvalho <yas@MacBook-Air-de-Yasmin.local>
2023-02-02 13:22:59 -0300

    feat: create class

---
+++ customer.py
@@ -0,0 +1,19 @@
+from person import Person
+from purchase import Purchase
+
+class Customer(Person):
+
+    def __init__(self,cpf,name,address,birth_date):
+        super.__init__(cpf,name,address,birth_date)
+
+    def pay_puchase(self, id):
+        for x in super().get_purchases():
+            if x.get_id() == id:
+                x.set_state("Finished")
+                break
+    
+    def cancel_purchase(self,cashier, id):
+        cashier.cancel_purchase(id)
+    
+    def new_purchase(self, products, cashier,employee):
+        pass
